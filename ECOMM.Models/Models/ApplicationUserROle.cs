f1d5d24be4cfbe1057ef6c853166145e63c21a85
MURAT CANDAŞ BOZYİĞİT <bozyigit31@gmail.com>
2024-10-11 17:36:02 +0300

    Tüm modelleri doldurdum şimdilik aklıma geren bağlantıları yaptım. Modeller bitti.

---
+++ ECOMM.Models/Models/ApplicationUserROle.cs
@@ -0,0 +1,20 @@
+﻿using ECOMM.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECOMM.Core.Models
+{
+    public class ApplicationUserRole
+    {
+
+        public string UserId { get; set; }
+        public User User { get; set; }
+
+
+        public string RoleId { get; set; }
+        public ApplicationRole Role { get; set; }
+    }
+}
---
+++ ECOMM.Models/Models/Comment.cs
@@ -0,0 +1,13 @@
+﻿
+using ECOMM.Core.Models;
+
+public class Comment : BaseModel
+{
+    public string? Content { get; set; }
+    public string AuthorId { get; set; }
+    public User Author { get; set; }
+    public DateTime DateCommented { get; set; } = DateTime.Now;
+
+    public int PostId { get; set; }
+    public Product Product { get; set; }
+}
