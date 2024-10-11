e609d904cfe05c80b979922c683399978c37063f
MURAT CANDAŞ BOZYİĞİT <bozyigit31@gmail.com>
2024-10-11 18:02:38 +0300

    TASK service ve interfacesini oluşturdum

---
+++ ECOMM.Business/Shared/Abstract/IService.cs
@@ -0,0 +1,19 @@
+﻿using ECOMM.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace ECOMM.Services.Abstract
+{
+    public interface IService<T> where T : BaseModel
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task<T> AddAsync(T entity);
+        Task<List<T>> AddRangeAsync(List<T> entities);
+        Task<T> UpdateAsync(T entity);
+        Task<bool> DeleteAsync(int id);
+        Task<T> GetFirstOrDefaultAsync(Expression<Func<T, bool>> predicate);
+    }
+}
---
+++ ECOMM.Business/Shared/Concrete/Servis.cs
@@ -0,0 +1,55 @@
+﻿using ECOMM.Data.Shared.Abstract;
+using ECOMM.Services.Abstract;
+using ECOMM.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace ECOMM.Services.Concrete
+{
+    public class Service<T> : IService<T> where T : BaseModel
+    {
+        private readonly IRepository<T> _repository;
+
+        public Service(IRepository<T> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _repository.GetAllAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _repository.GetByIdAsync(id);
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            return await _repository.AddAsync(entity);
+        }
+
+        public async Task<List<T>> AddRangeAsync(List<T> entities)
+        {
+            return await _repository.AddRangeAsync(entities);
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+            return await _repository.UpdateAsync(entity);
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            return await _repository.DeleteAsync(id);
+        }
+
+        public async Task<T> GetFirstOrDefaultAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _repository.GetFirstOrDefaultAsync(predicate);
+        }
+    }
+}
