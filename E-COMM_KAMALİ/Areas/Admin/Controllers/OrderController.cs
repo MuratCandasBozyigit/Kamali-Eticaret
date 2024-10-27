36975fe72d243e45e280d2b91ecb7c1a9743ab2a
Murat Candaş Bozyiğit <105310971+MuratCandasBozyigit@users.noreply.github.com>
2024-10-28 00:45:31 +0300

    Controllerı actım.

---
+++ E-COMM_KAMALİ/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ECOMM.Business;
+using ECOMM.Business.Abstract;
+namespace E_COMM_KAMALİ.Areas.Admin.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderService _orderService;
+        private readonly ICategoryService _categoryService;
+        private readonly IProductService _productService;
+        private readonly IUserService _userService;
+        public OrderController(IOrderService orderService, ICategoryService categoryService, IProductService productService, IUserService userService)
+        {
+            _orderService = orderService;
+            _categoryService = categoryService;
+            _productService = productService;
+            _userService = userService;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
