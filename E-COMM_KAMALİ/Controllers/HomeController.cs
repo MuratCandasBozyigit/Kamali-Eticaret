6f7578691b2b38c8de3da9daf91c8534ab54ccc3
Murat Candaş Bozyiğit <105310971+MuratCandasBozyigit@users.noreply.github.com>
2024-11-02 19:34:05 +0300

    olmuyooo geri tuşu calışmyor

--- E-COMM_KAMALİ/Controllers/HomeController.cs
+++ E-COMM_KAMALİ/Controllers/HomeController.cs
@@ -2,6 +2,15 @@ using E_COMM_KAMALİ.Models;
 using ECOMM.Business.Abstract;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Http; // Eklenmeli
+
+public static class HttpRequestExtensions
+{
+    public static bool IsAjaxRequest(this HttpRequest request)
+    {
+        return request.Headers["X-Requested-With"] == "XMLHttpRequest";
+    }
+}
 
 namespace E_COMM_KAMALİ.Controllers
 {
@@ -13,6 +22,7 @@ namespace E_COMM_KAMALİ.Controllers
         private readonly IOrderService _orderService;
         // private readonly IFavouritesService _favouritesService;
         private readonly ICommentService _commentService;
+
         public HomeController(ILogger<HomeController> logger, IProductService productService, IOrderService orderService, /*IFavouritesService favouritesService,*/ ICommentService commentService)
         {
             _logger = logger;
@@ -20,24 +30,40 @@ namespace E_COMM_KAMALİ.Controllers
             _orderService = orderService;
             //  _favouritesService = favouritesService;
             _commentService = commentService;
-        } 
+        }
         #endregion
-
 
         public async Task<IActionResult> Index(int page = 1)
         {
             int pageSize = 4;
-            var products = await _productService.GetAllAsync();
-         
-            var paginatedProducts = products.Skip((page - 1) * pageSize)
-                                            .Take(pageSize)
-                                            .ToList();
+            try
+            {
+                var products = await _productService.GetAllAsync();
+                var paginatedProducts = products.Skip((page - 1) * pageSize)
+                                                .Take(pageSize)
+                                                .ToList();
 
-            ViewBag.CurrentPage = page;
-            ViewBag.TotalPages = Math.Ceiling((double)products.Count() / pageSize);
+                ViewBag.CurrentPage = page;
+                ViewBag.TotalPages = Math.Ceiling((double)products.Count() / pageSize);
 
-            return View(paginatedProducts);
+
+                // Eğer AJAX isteği ise, yalnızca ürün listesini döndür
+                if (Request.IsAjaxRequest())
+                {
+                    return PartialView("_ProductListPartial", paginatedProducts); // Yalnızca ürün listesini döndür
+                }
+
+                // Tam sayfa görünüm döndür
+                return View(paginatedProducts);
+            }
+            catch (Exception ex)
+            {
+                // Hata kaydı yap
+                _logger.LogError(ex, "Ürünler yüklenirken hata oluştu.");
+                return StatusCode(500, "Ürünler yüklenirken bir hata oluştu.");
+            }
         }
+
 
 
         public async Task<IActionResult> GetAllCategories()
@@ -47,23 +73,13 @@ namespace E_COMM_KAMALİ.Controllers
             return View(categories);
         }
 
-
-
-
-
-
-
-
-
-
-
         #region s
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        } 
+        }
         #endregion
     }
 }
