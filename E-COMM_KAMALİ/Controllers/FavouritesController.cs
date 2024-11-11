c2673f6b06c7174354ec69a9c051895764fc234b
Murat Candaş Bozyiğit <105310971+MuratCandasBozyigit@users.noreply.github.com>
2024-11-11 18:41:52 +0300

    Favoriler tuşları düzenledim

--- E-COMM_KAMALİ/Controllers/FavouritesController.cs
+++ E-COMM_KAMALİ/Controllers/FavouritesController.cs
@@ -1,4 +1,6 @@
 ﻿using ECOMM.Business.Abstract;
+using ECOMM.Business.Concrete;
+using ECOMM.Core.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace E_COMM_KAMALİ.Controllers
@@ -10,20 +12,43 @@ namespace E_COMM_KAMALİ.Controllers
     private readonly IProductService productService;
         private readonly ICategoryService categoryService;
         private readonly ISubCategoryService subCategoryService;
-
-        public FavouritesController(IUserService userService, IProductService productService, ISubCategoryService subCategoryService, ICategoryService categoryService, IFavouritesService favouritesService)
+        private readonly ISessionService _sessionService;
+        public FavouritesController(IUserService userService, IProductService productService, ISubCategoryService subCategoryService, ICategoryService categoryService, IFavouritesService favouritesService, ISessionService sessionService)
         {
             this.userService = userService;
             this.productService = productService;
             this.subCategoryService = subCategoryService;
             this.categoryService = categoryService;
+            _sessionService = sessionService;
             //  this.favouritesService = favouritesService;
         }
 
 
         public IActionResult Index()
         {
-            return View();
+            var cartItems = _sessionService.GetCartItems(); // Oturumdan sepet öğelerini al
+            return View(cartItems); // Sepet sayfasına gönder
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddToCart(int productId, int quantity)
+        {
+            var product = await productService.GetByIdAsync(productId); // Ürünü al
+            if (product != null)
+            {
+                var cartItem = new CartItemViewModel
+                {
+                    ProductId = product.Id,
+                    ProductName = product.ProductName,
+                    ImagePath = product.ImagePath,
+                    Price = product.ProductPrice,
+                    Quantity = quantity
+                };
+
+                _sessionService.AddToCart(cartItem); // Sepete ekle
+            }
+
+            return RedirectToAction("Index", "Favourites"); // Sepet sayfasına yönlendir
         }
     }
 }
