25e34b51cc9ebe8392136af08f225ba9f0709b25
Murat Candaş Bozyiğit <105310971+MuratCandasBozyigit@users.noreply.github.com>
2024-11-15 03:38:42 +0300

    l

---
+++ E-COMM_KAMALİ/Controllers/CheckoutController.cs
@@ -0,0 +1,26 @@
+﻿using ECOMM.Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_COMM_KAMALİ.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        private readonly IOrderService _orderService;
+        private readonly IUserService _userService;
+        private readonly IProductService _productService;
+
+        public CheckoutController(IProductService productService, IUserService userService, IOrderService orderService)
+        {
+            _productService = productService;
+            _userService = userService;
+            _orderService = orderService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Chat-Gpt ye nasıl yapılması gerektiğini sor "bankalar nasıl istiyor neleri ypamam lazımki bankalar kabul etsin
+    }
+}
