018dad297b00e55384d5fd38006d7c1de8355fc5
Murat Candaş Bozyiğit <105310971+MuratCandasBozyigit@users.noreply.github.com>
2024-11-17 22:23:45 +0300

    s

---
+++ ECOMM.Models/ViewModels/HomeViewModel.cs
@@ -0,0 +1,21 @@
+﻿using ECOMM.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    namespace ECOMM.Core.ViewModels
+    {
+        public class HomeViewModel
+        {
+            public List<Product> Products { get; set; }
+            public List<Category> Categories { get; set; }
+            public int TotalCount { get; set; }
+            public int PageSize { get; set; } = 6; // Sayfada gösterilecek ürün sayısı
+            public int CurrentPage { get; set; } = 1; // Mevcut sayfa
+            public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
+        }
+    }
+
