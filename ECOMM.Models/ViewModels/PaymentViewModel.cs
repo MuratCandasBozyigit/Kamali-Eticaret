5bb5cf889e8953c42ab040c0599baf6d0c81873d
Murat Candaş Bozyiğit <105310971+MuratCandasBozyigit@users.noreply.github.com>
2024-11-19 03:59:49 +0300

    PaymentViewModel

---
+++ ECOMM.Models/ViewModels/PaymentViewModel.cs
@@ -0,0 +1,18 @@
+﻿using ECOMM.Core.Models;
+using ECOMM.Core.ViewModels;
+
+public class PaymentViewModel
+{
+    public string UserId { get; set; }
+    public string UserName { get; set; }
+    public string Email { get; set; }
+
+    public string ShippingAddress { get; set; }
+    public string City { get; set; }
+    public string PostalCode { get; set; }
+
+    public PaymentMethodEnum PaymentMethod { get; set; }
+
+    public List<CartItemViewModel> CartItems { get; set; } = new List<CartItemViewModel>();
+    public decimal TotalAmount { get; set; }
+}
