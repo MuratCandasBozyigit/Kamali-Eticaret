f140c9d99a3c8cabdfca725b5decec1fa6e9b6bc
Murat Candaş Bozyiğit <105310971+MuratCandasBozyigit@users.noreply.github.com>
2024-11-19 03:57:49 +0300

    Servisleri ekledim constructer ekledim.

--- E-COMM_KAMALİ/Controllers/OrderController.cs
+++ E-COMM_KAMALİ/Controllers/OrderController.cs
@@ -1,9 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ECOMM.Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
 
 namespace E_COMM_KAMALİ.Controllers
 {
     public class OrderController : Controller
     {
+        private readonly IOrderService _orderService;
+        private readonly IProductService _productSercice;
+        private readonly IUserService _userService;
+
+        public OrderController(IUserService userService, IProductService productSercice, IOrderService orderService)
+        {
+            _userService = userService;
+            _productSercice = productSercice;
+            _orderService = orderService;
+        }
+
         public IActionResult Index()
         {
             return View();
