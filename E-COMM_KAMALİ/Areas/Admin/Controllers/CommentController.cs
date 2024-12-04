43f666e4be0a326e6b8a3683165aa0966e23d685
Murat Candaş Bozyiğit <105310971+MuratCandasBozyigit@users.noreply.github.com>
2024-12-05 00:31:30 +0300

    Comment controlleları ekledim.

---
+++ E-COMM_KAMALİ/Areas/Admin/Controllers/CommentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_COMM_KAMALİ.Areas.Admin.Controllers
+{
+    public class CommentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ E-COMM_KAMALİ/Controllers/CommentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_COMM_KAMALİ.Controllers
+{
+    public class CommentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
