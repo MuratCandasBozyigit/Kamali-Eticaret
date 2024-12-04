8b4d21756faeb7e98b82d68d9afc28176507cb7b
Murat Candaş Bozyiğit <105310971+MuratCandasBozyigit@users.noreply.github.com>
2024-12-05 00:47:33 +0300

    Controllerlara servisleri ekleidm

--- E-COMM_KAMALİ/Areas/Admin/Controllers/CommentController.cs
+++ E-COMM_KAMALİ/Areas/Admin/Controllers/CommentController.cs
@@ -1,9 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ECOMM.Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
 
 namespace E_COMM_KAMALİ.Areas.Admin.Controllers
 {
     public class CommentController : Controller
     {
+        private readonly ICommentService commentService;
+        private readonly IUserService userService;
+        private readonly IProductService productService;
+        private IOrderService orderService;
+
+        public CommentController(IOrderService orderService, IProductService productService, IUserService userService, ICommentService commentService)
+        {
+            this.orderService = orderService;
+            this.productService = productService;
+            this.userService = userService;
+            this.commentService = commentService;
+        }
+
         public IActionResult Index()
         {
             return View();
