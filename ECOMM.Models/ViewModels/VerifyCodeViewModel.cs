0b7f7b117903aa05f38f2d5dc5334e36751e4715
Murat Candaş Bozyiğit <105310971+MuratCandasBozyigit@users.noreply.github.com>
2024-12-06 02:57:36 +0300

    s

---
+++ ECOMM.Models/ViewModels/VerifyCodeViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECOMM.Core.ViewModels
+{
+    public class VerifyCodeViewModel
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(6, MinimumLength = 6, ErrorMessage = "Verification code must be 6 digits.")]
+        public string VerificationCode { get; set; }
+    }
+
+}
