66046cf80eeb186b1a8aa86264e802fef8ed8c70
Murat Candaş Bozyiğit <105310971+MuratCandasBozyigit@users.noreply.github.com>
2024-12-10 02:30:16 +0300

    Yeni Migration

--- ECOMM.DATA/Migrations/20241209220228_Inıtial.Designer.cs
+++ ECOMM.DATA/Migrations/20241209220228_Inıtial.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ECOMM.Data.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    [Migration("20241208210811_Inıt1")]
-    partial class Inıt1
+    [Migration("20241209220228_Inıtial")]
+    partial class Inıtial
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -325,14 +325,9 @@ namespace ECOMM.Data.Migrations
                         .IsRequired()
                         .HasColumnType("nvarchar(450)");
 
-                    b.Property<string>("UserId1")
-                        .HasColumnType("nvarchar(450)");
-
                     b.HasKey("Id");
 
                     b.HasIndex("UserId");
-
-                    b.HasIndex("UserId1");
 
                     b.ToTable("Orders");
                 });
@@ -696,14 +691,10 @@ namespace ECOMM.Data.Migrations
             modelBuilder.Entity("ECOMM.Core.Models.Orders", b =>
                 {
                     b.HasOne("ECOMM.Core.Models.User", "User")
-                        .WithMany()
+                        .WithMany("UserOrders")
                         .HasForeignKey("UserId")
                         .OnDelete(DeleteBehavior.Cascade)
                         .IsRequired();
-
-                    b.HasOne("ECOMM.Core.Models.User", null)
-                        .WithMany("UserOrders")
-                        .HasForeignKey("UserId1");
 
                     b.Navigation("User");
                 });
