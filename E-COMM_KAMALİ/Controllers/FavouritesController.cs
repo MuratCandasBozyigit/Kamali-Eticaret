df4d7437cf145b4b04a51340848a42f6f6e30ccf
Murat Candaş Bozyiğit <105310971+MuratCandasBozyigit@users.noreply.github.com>
2024-12-17 14:03:50 +0300

    Koy gö/e

--- E-COMM_KAMALİ/Controllers/FavouritesController.cs
+++ E-COMM_KAMALİ/Controllers/FavouritesController.cs
@@ -34,26 +34,30 @@ namespace E_COMM_KAMALİ.Controllers
             return View(favouritesItems); // Favori sayfasına gönder
         }
 
+
+
         // Favorilere ürün ekle
         [HttpPost]
-        public async Task<IActionResult> AddToFavourites(int productId,string selectedSize)
+        public async Task<IActionResult> AddToFavourites(int productId, string selectedSize)
         {
-            var product = await productService.GetByIdAsync(productId); // Ürünü al
+           
+
+            var product = await productService.GetByIdAsync(productId);
             if (product != null)
             {
                 var cartItem = new CartItemViewModel
                 {
                     ProductId = product.Id,
                     ProductName = product.ProductName,
-                    ProductSize = selectedSize, // Sadece seçilen bedeni kullan
+                    ProductSize = selectedSize,
                     ImagePath = product.ImagePath,
                     Price = product.ProductPrice,
                 };
 
-                _sessionService.AddToFavourites(cartItem); // Favorilere ekle
+                _sessionService.AddToFavourites(cartItem);
             }
 
-            return RedirectToAction("Index", "Favourites"); // Favori sayfasına yönlendir
+            return RedirectToAction("Index", "Favourites");
         }
 
 
